86233886974e8105acef6878260baec7366844ed
ap15114 <r.nambiar@se15.qmul.ac.uk>
2016-01-15 14:51:12 +0000

    Added another line of text using 'cout'

--- 2. MySecondProject/main.cpp
+++ 2. MySecondProject/main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main() {
    string message = "This is a message.";
+    string message2 = "This is another message.";
     cout << message << endl;
+    cout << message2 << endl;
     return 0;
 }
