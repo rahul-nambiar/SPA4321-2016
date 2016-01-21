2590dc4fd7c18f250f5d2fe962cb93904c3a91dc
ap15114 <r.nambiar@se15.qmul.ac.uk>
2016-01-21 16:40:29 +0000

    Section A (Part 2) - Exercise 2 Use console input and assign values to variables.

---
+++ 5. BuiltInTypes/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i = i;
+    cin >> i;
+    cout << i << endl;
+
+    char c = c;
+    cin >> c;
+    cout << c << endl;
+
+    bool condition = condition;
+    cin >> condition;
+    cout << condition << endl;
+
+    float x = x;
+    cin >> x;
+    cout << x << endl;
+
+    double y = y;
+    cin >> y;
+    cout << y << endl;
+
+    return 0;
+}
