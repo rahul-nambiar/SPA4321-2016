3feca52f8739432c63f695e6d8eee9229f23ee6b
ap15114 <r.nambiar@se15.qmul.ac.uk>
2016-01-21 16:43:02 +0000

    Section A (Part 2) - Exercise 4 - Simple Maths Basic Mathematical Operations

---
+++ 7. SimpleMaths/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Addition" << endl;
+    int x = x;
+    cin >> x;
+    int y = y;
+    cin >> y;
+    float z = float(x)+y;
+
+    cout << x << " " << "+" << " " << y << " " << "=" << " " << z << endl;
+    cout << endl;
+
+    cout << "Multiplication" << endl;
+    int u = u;
+    cin >> u;
+    int v = v;
+    cin >> v;
+    float w = float(u)*v;
+
+    cout << u << " " << "*" << " " << v << " " << "=" << " " << w << endl;
+    cout << endl;
+
+    cout << "Division" << endl;
+    int r = r;
+    cin >> r;
+    int s = s;
+    cin >> s;
+    float t = float(r)/s;
+
+    cout << r << " " << "/" << " " << s << " " << "=" << " " << t << endl;
+    cout << endl;
+
+    cout << "Subtraction" << endl;
+    int n = n;
+    cin >> n;
+    int p = p;
+    cin >> p;
+    float q = float(n)-p;
+
+    cout << n << " " << "-" << " " << p << " " << "=" << " " << q << endl;
+    cout << endl;
+
+    cout << "Modulo" << endl; //Remainder Calculation
+    int k = k;
+    cin >> k;
+    int l = l;
+    cin >> l;
+    float m = k%l;
+
+    cout << k << " " << "%" << " " << l << " " << "=" << " " << m << endl;
+    cout << endl;
+}
