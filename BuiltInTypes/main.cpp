cee5ef688223bed842c7ee28d27204036bcff1dd
ap15114 <r.nambiar@se15.qmul.ac.uk>
2016-01-21 14:13:49 +0000

    Section A (Part 2) - Exercise 1 Assign values to BuildInTypes and prints them to console.

---
+++ BuiltInTypes/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i = 1;
+    char c = 'X';
+    bool condition = true;
+    float x = 5.0;
+    double y = 5.012315658;
+    cout << i << endl;
+    cout << c << endl;
+    cout << condition << endl;
+    cout << x << endl;
+    cout << y << endl;
+    return 0;
+}
