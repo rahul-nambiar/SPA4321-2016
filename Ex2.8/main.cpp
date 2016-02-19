dcd7c42940fe7984fd01c3b36f6092ba6e0b7701
Rahul <r.nambiar@se15.qmul.ac.uk>
2016-02-19 17:01:02 +0000

    Added second shape - smaller red square

--- Ex2.8/main.cpp
+++ Ex2.8/main.cpp
@@ -20,11 +20,15 @@ int main(int argc, char * argv[]) {
 
 	// Add code here to draw things
 	Shape * shape = new Shape(250,250,0);
-	shape->setRotationSpeed(2); // initial rotation speed in degrees
+	Shape * shape1 = new Shape(250,250,45);
+	shape->setRotationSpeed(2);
+	shape1->setRotationSpeed(-2);// initial rotation speed in degrees
 	// make a new line
 	Line * line = new Line();
+	Line * line1 = new Line();
 	// allowed colours are specified in the enum in Colours.hpp
 	line->setColor(getColor(BLACK));
+	line1->setColor(getColor(RED));
 	// add a point to the line
 	// these points are all relative to the centre of the Shape
 	line->addPoint(Point(-100,-100));
@@ -32,10 +36,18 @@ int main(int argc, char * argv[]) {
 	line->addPoint(Point(100,100));
 	line->addPoint(Point(-100,100));
 	line->addPoint(Point(-100,-100));
+
+	line1->addPoint(Point(-50,-50));
+	line1->addPoint(Point(50,-50));
+	line1->addPoint(Point(50,50));
+	line1->addPoint(Point(-50,50));
+	line1->addPoint(Point(-50,-50));
 	// add this line to the shape
 	shape->addLine(line);
+	shape1->addLine(line1);
 	// add the shape to the window
 	window.addShape(shape);
+	window.addShape(shape1);
 	// notice the use of . notation for the window
 	// and contrast with the use of -> notation for the shape and line
 
