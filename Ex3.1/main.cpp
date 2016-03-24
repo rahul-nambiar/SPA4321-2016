a5ae313e65696f4aee2f29088deb693ad34002ae
ap15114 <r.nambiar@se15.qmul.ac.uk>
2016-03-24 14:47:06 +0000

    Added random.cpp and random.h files

--- Ex3.1/main.cpp
+++ Ex3.1/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "functions.h"
+#include "random.h"
+#include <cmath>
 
 using namespace std;
 
@@ -20,6 +22,19 @@ int main() {
     else {
         cout << "Test 2 FAILED" << endl;
     }
+
+    double speed = 10.0;
+    double distance = 5.0;
+    double g = 10.0;
+
+    double nominalAngle = angle(distance,speed);
+    cout << nominalAngle << endl;
+
+    double pi = 3.141592654;
+    double deltaAngle = Random::randomNorm(0,(2*(pi/180)));
+
+    double actualAngle = nominalAngle + deltaAngle;
+    cout << actualAngle << endl;
 
     return 0;
 }
