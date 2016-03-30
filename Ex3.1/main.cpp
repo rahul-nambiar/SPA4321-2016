bda45f853f0b6becb071b218443c36881828c6c2
Rahul <r.nambiar@se15.qmul.ac.uk>
2016-03-30 16:18:00 +0100

    Added code to main.cpp

--- Ex3.1/main.cpp
+++ Ex3.1/main.cpp
@@ -25,16 +25,31 @@ int main() {
 
     double speed = 10.0;
     double distance = 5.0;
-    double g = 10.0;
 
     double nominalAngle = angle(distance,speed);
     cout << nominalAngle << endl;
 
     double pi = 3.141592654;
-    double deltaAngle = Random::randomNorm(0,(2*(pi/180)));
+    double sigma1 = (pi/180)*2;
+
+    double deltaAngle = Random::randomNorm(0,sigma1);
+    cout << deltaAngle << endl;
 
     double actualAngle = nominalAngle + deltaAngle;
     cout << actualAngle << endl;
 
+    double deltaSpeed = Random::randomNorm(1,0.1);
+
+    double actualSpeed = speed*deltaSpeed;
+
+    int hits = 0;
+
+    for (int i = 0; i < 10001; ++i) {
+        double height = impact(actualAngle,actualSpeed,distance);
+        if(height<0.5,height>-0.5){
+            hits++;
+        }
+    }
+cout << hits;
     return 0;
 }
