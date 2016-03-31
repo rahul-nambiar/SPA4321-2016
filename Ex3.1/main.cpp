535ccf2fd8eb369f73f0886be60e355b74e8e1ba
ap15114 <r.nambiar@se15.qmul.ac.uk>
2016-03-31 16:40:29 +0100

    Completed Part E

--- Ex3.1/main.cpp
+++ Ex3.1/main.cpp
@@ -10,7 +10,7 @@ int main() {
 
 srand(time(0));
 time_t t = time(0);
-cout << t << endl;
+cout << "Seed = " << t << endl;
 
     bool t1 = test1();
     if (t1){
@@ -33,25 +33,35 @@ cout << t << endl;
     double distance = 5.0;
 
     double nominalAngle = angle(distance,speed);
-    cout << nominalAngle << endl;
 
     double pi = 3.141592654;
     double sigma1 = 2*(pi/180);
 
+    int wins = 0;
+    int hitsInARow = 0;
+
 for (int i = 0; i < 10000; ++i) {
 
-    double deltaAngle = Random::randomNorm(0,sigma1);
+    double deltaAngle = Random::randomNorm(0, sigma1);
     double actualAngle = nominalAngle + deltaAngle;
 
-    double deltaSpeed = Random::randomNorm(1,0.1);
-    double actualSpeed = speed*deltaSpeed;
+    double deltaSpeed = Random::randomNorm(1, 0.1);
+    double actualSpeed = speed * deltaSpeed;
 
-    double height = impact(actualAngle,actualSpeed,distance);
+    double height = impact(actualAngle, actualSpeed, distance);
 
-    if(-0.5<height<0.5){
+    if (-0.5 < height < 0.5) {
         hits++;
+        hitsInARow++;
+    }
+    else{hitsInARow=0;}
+
+    if (hitsInARow==3){
+        wins++;
+        hitsInARow=0;
     }
 }
-cout << hits;
-    return 0;
+    cout << "Hits = " << hits << endl;
+    cout << "Wins = " << wins << endl;
+return 0;
 }
